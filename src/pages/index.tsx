4bab87e927f3d20aa2e327162aaac7ba46080c7b
Higor Allan <higor.allan21@gmail.com>
2022-02-16 09:06:45 -0300

    comeco da aplicacao graphq

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import {GetServerSideProps} from 'next';
+import {usePageQuery, PageDocument} from '../generated/graphql';
+import {client, ssrCache} from '../lib/urql';
+
+export default function Home() {
+	const [{data}] = usePageQuery({
+		variables: {
+			slug: 'about',
+		},
+	});
+	return (
+		<div>
+			<h1>{data?.page.title}</h1>
+		</div>
+	);
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	await client.query(PageDocument, {slug: 'about'}).toPromise();
+
+	return {
+		props: {
+			urqlState: ssrCache.extractData(),
+		},
+	};
+};
